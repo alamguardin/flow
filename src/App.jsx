a126e088b6100cccacef880ec1f777c9d0c80cd4
Alam Guardin <alamguardin@gmail.com>
2024-04-26 22:25:03 -0500

    chore: add config files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+    <h1 className="text-2xl font-semibold text-slate-900">This is my count: {count}</h1>
+    <button 
+      className="text-xs font-semibold bg-violet-500 text-violet-50 rounded-lg px-5 py-2 mt-4 font-sans" 
+      onClick={() => setCount(count + 1)}
+    >Click me</button>
+   </>
   )
 }
 
