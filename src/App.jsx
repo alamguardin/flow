a1d3172b283f15f826285fc08c6b52cc45b12a40
Alam Guardin <alamguardin@gmail.com>
2024-05-13 20:20:59 -0500

    style: change colors

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
    <>
     <CreateGroupForm></CreateGroupForm>
-    <h1 className="text-2xl font-semibold text-slate-900">This is my count: {count}</h1>
+    <h1 className="text-2xl font-semibold text-slate-50">This is my count: {count}</h1>
     <button 
       className="text-xs font-semibold bg-violet-500 text-violet-50 rounded-lg px-5 py-2 mt-4 font-sans" 
       onClick={() => setCount(count + 1)}
--- src/components/CreateGroupForm.jsx
+++ src/components/CreateGroupForm.jsx
@@ -1,15 +1,15 @@
 export default function CreateGroupForm() {
     return (
         <div className="max-w-lg m-auto my-9">
-            <h1 className="font-bold text-slate-950 text-4xl mb-2">Flow</h1>
+            <h1 className="font-bold text-slate-50 text-4xl mb-2">Flow</h1>
             <div className="flex gap-4">
                 <input 
                     type="text"
-                    className="w-full py-2 px-3 outline-none bg-slate-50 border-2 border-slate-200 rounded-md font-sans text-sm font-semibold text-slate-800 focus:border-blue-300"
-                    placeholder="Type a name group"
+                    className="w-full py-2 px-3 outline-none bg-slate-900 border-2 border-slate-800 rounded-md font-sans text-sm font-semibold text-slate-300 focus:border-violet-500"
+                    placeholder="Type your task"
                 />
                 <button
-                    className="py-2 px-3 bg-violet-500 outline-none rounded-md font-sans text-sm font-semibold text-slate-50"
+                    className="py-2 px-3 bg-violet-500 outline-none rounded-md font-sans text-sm font-semibold text-slate-50 hover:bg-violet-400 active:bg-violet-300"
                 >Create</button>
             </div>
         </div>
