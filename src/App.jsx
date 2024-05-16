837936900a69392988348ad49d5b060fad65c8a7
Alam Guardin <alamguardin@gmail.com>
2024-05-15 20:51:15 -0500

    chore: restart project using bun

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import CreateGroupForm from './components/CreateGroupForm'
 
 function App() {
-  const [list, setList] = useState([])
-
-  function addTask(task) {
-    const objTask = {
-      id: Math.floor(Math.random(1) * 100),
-      content: task
-    }
-
-    let newList = list
-    newList.push(objTask)
-    console.log(newList)
-    setList(newList)
-  }
-
-  const tasksList = list.map(task => 
-    <li key={task.id}>{task.content}</li>
-  )
+  const [count, setCount] = useState(0)
 
   return (
-   <>
-    <CreateGroupForm action={addTask}></CreateGroupForm>
-    <ul>{tasksList}</ul>
-   </>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
