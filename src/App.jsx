19f1896fa2ef1bf7f50b4b3b12a2e193e7280352
Alam Guardin <alamguardin@gmail.com>
2024-05-26 14:46:41 -0500

    feat: show tasks list into dom

--- src/App.jsx
+++ src/App.jsx
@@ -30,21 +30,25 @@ function App() {
             </header>
             <div className="list container">
                 <ul className="list-container">
-                    <li className="item">
-                        <input type="checkbox" className="item-checkbox"/>
-                        <span className="item-text">This is my task</span>
-                        <div className="dropdown">
-                            <button className="dropdown-button">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5 10C3.9 10 3 10.9 3 12C3 13.1 3.9 14 5 14C6.1 14 7 13.1 7 12C7 10.9 6.1 10 5 10ZM19 10C17.9 10 17 10.9 17 12C17 13.1 17.9 14 19 14C20.1 14 21 13.1 21 12C21 10.9 20.1 10 19 10ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10Z"></path></svg>
-                            </button>
-                            <div className="dropdown-content">
-                                <a href="#" className="dropdown-item">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 4V6H15V4H9Z"></path></svg>
-                                    <span>Delete</span>
-                                </a>
-                            </div>
-                        </div>
-                    </li>
+                    {
+                        list.map(item =>
+                            <li className="item" key={item.id}>
+                                <input type="checkbox" className="item-checkbox"/>
+                                <span className="item-text">{item.description}</span>
+                                <div className="dropdown">
+                                    <button className="dropdown-button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5 10C3.9 10 3 10.9 3 12C3 13.1 3.9 14 5 14C6.1 14 7 13.1 7 12C7 10.9 6.1 10 5 10ZM19 10C17.9 10 17 10.9 17 12C17 13.1 17.9 14 19 14C20.1 14 21 13.1 21 12C21 10.9 20.1 10 19 10ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10Z"></path></svg>
+                                    </button>
+                                    <div className="dropdown-content">
+                                        <a href="#" className="dropdown-item">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 4V6H15V4H9Z"></path></svg>
+                                            <span>Delete</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    }
                 </ul>
             </div>
         </>
