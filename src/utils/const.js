47ffdf051571351eae963c6c1c977024849be577
Alam Guardin <alamguardin@gmail.com>
2024-12-25 17:03:26 -0500

    Reset Project

--- src/utils/const.js
+++
@@ -1,6 +0,0 @@
-export const API_URL_POST = import.meta.env.VITE_API_URL_POST
-export const API_URL_GET = import.meta.env.VITE_API_URL_GET
-export const API_URL_PATCH = import.meta.env.VITE_API_URL_PATCH
-export const API_KEY = import.meta.env.VITE_API_KEY
-export const API_AUTHORIZATION = import.meta.env.VITE_API_AUTHORIZATION
-
